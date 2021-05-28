a3ad0d02bba35def37a5b1d0ca1c09f72d067c67
ZeeshanSalem <zeeshansaleem729@gmail.com>
2021-05-28 10:40:15 +0500

    task is completed

---
+++ lib/core/models/cart.g.dart
@@ -0,0 +1,49 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'cart.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class CartAdapter extends TypeAdapter<Cart> {
+  @override
+  final int typeId = 0;
+
+  @override
+  Cart read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return Cart()
+      ..id = fields[0] as int
+      ..name = fields[1] as String
+      ..price = fields[2] as String
+      ..img = fields[3] as String;
+  }
+
+  @override
+  void write(BinaryWriter writer, Cart obj) {
+    writer
+      ..writeByte(4)
+      ..writeByte(0)
+      ..write(obj.id)
+      ..writeByte(1)
+      ..write(obj.name)
+      ..writeByte(2)
+      ..write(obj.price)
+      ..writeByte(3)
+      ..write(obj.img);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is CartAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
